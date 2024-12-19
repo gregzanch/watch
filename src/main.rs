324e02a1c0c015c17457332c76509e724721c46c
gregzanch <zanchelli.greg@gmail.com>
2024-12-19 13:53:50 +0530

    Allow for command usage

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
+use std::io::Write;
+use std::process::{Command, Stdio};
 use std::time::Duration;
-use std::{fs, thread};
 use std::{
     collections::HashMap,
     io::{self, Read},
     time::SystemTime,
 };
+use std::{env, fs, process, thread};
 
 #[derive(Debug)]
 struct State {
     files: HashMap<String, u128>,
+    command: String,
+    command_args: Vec<String>,
 }
 
 impl State {
     fn new() -> Self {
         Self {
-            files: HashMap::new()
+            files: HashMap::new(),
+            command: String::new(),
+            command_args: Vec::new(),
         }
     }
     fn check(&mut self) -> bool {
@@ -55,8 +61,38 @@ fn read_input() -> Vec<String> {
         .collect()
 }
 
+fn parse_args(state: &mut State) {
+    let args: Vec<String> = env::args().collect();
+    let help_text = include_str!("../help.txt");
+    match args.len() {
+        1 => {
+            println!("No command found\n");
+            println!("{}", help_text);
+            process::exit(1);
+        }
+        2 => {
+            let split_command_string: Vec<String> = args[1]
+                .clone()
+                .split(" ")
+                .map(str::to_string)
+                .filter(|x| !x.is_empty())
+                .collect();
+            let command = split_command_string.first().expect("Could not parse command");
+            let command_args = split_command_string.split_at(1).1;
+            state.command = command.to_owned();
+            state.command_args = Vec::from(command_args);
+        }
+        _ => {
+            println!("Invalid usage...\n");
+            println!("{}", help_text);
+            process::exit(1);
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     let mut state = State::new();
+    parse_args(&mut state);
     for file_path in read_input() {
         let last_modified = get_last_modified(&file_path);
         state.files.insert(file_path, last_modified);
@@ -64,8 +100,22 @@ fn main() -> io::Result<()> {
     loop {
         thread::sleep(Duration::from_millis(100));
         if state.check() {
-            // run the desired command
-            println!("changed");
+            let mut child = Command::new(&state.command)
+                .args(&state.command_args)
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect(format!("Could not initiate command: {}", state.command).as_str());
+            let mut stdout = child.stdout.take().unwrap();
+
+            let mut buf = Vec::new();
+
+            stdout
+                .read_to_end(&mut buf)
+                .expect("Failed to read child's stdout");
+            std::io::stdout()
+                .write(&buf)
+                .expect("Failed to write child's stdout");
         }
     }
 }
